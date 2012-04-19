991f0a079f993b1b3c4fc802aca2e8a3d3d41ba8
Matt Westhoff <mattwesthoff@gmail.com>
2012-04-19 11:52:49 -0500

    drag over and it changes styles!

--- dragonupload/Scripts/Home/index.js
+++ dragonupload/Scripts/Home/index.js
@@ -1,13 +1,24 @@
-﻿function dragEnter() {
-    $("#msg").text("you're dragging things!");
+﻿function dragEnter(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(event.target).addClass('dragging');
 }
 
-function dragLeave() {
-    $("#msg").text("aww come back!");
+function dragLeave(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(event.target).removeClass('dragging');
+}
+
+function drop(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(event.target).removeClass('dragging');
 }
 
 $(document).ready(function () {
     var $box = $("#dropbox");
     $box.bind("dragenter", dragEnter);
     $box.bind("dragleave", dragLeave);
+    $box.bind("drop", drop);
 });
