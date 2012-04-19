f5a68b199f639d7a4f7c96695a8eb8c526c84449
Matt Westhoff <mattwesthoff@gmail.com>
2012-04-19 11:52:35 -0500

    a page, some styles, and some scripts

---
+++ dragonupload/Scripts/Home/index.js
@@ -0,0 +1,13 @@
+﻿function dragEnter() {
+    $("#msg").text("you're dragging things!");
+}
+
+function dragLeave() {
+    $("#msg").text("aww come back!");
+}
+
+$(document).ready(function () {
+    var $box = $("#dropbox");
+    $box.bind("dragenter", dragEnter);
+    $box.bind("dragleave", dragLeave);
+});
